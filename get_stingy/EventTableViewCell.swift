ef9ba133776dd763ce8a71d3c9acfec11073a3d2
nabilaherzegovina <s3463558@student.rmit.edu.au>
2017-08-05 21:14:07 +1000

    first initial commit with some table view cells dummy going on

---
+++ get_stingy/EventTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  EventTableViewCell.swift
+//  get_stingy
+//
+//  Created by Nabila on 05/08/2017.
+//  Copyright © 2017 Nabila Herzegovina. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+	@IBOutlet weak var cellTitle: UILabel!
+	
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- get_stingy/ViewController.swift
+++ get_stingy/ViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
+	@IBOutlet weak var eventTable: UITableView!
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		eventTable.delegate = self
+		eventTable.dataSource = self
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+	
+}
 
-
+extension ViewController: UITableViewDataSource {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 7;
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let nib = UINib(nibName: "EventTableViewCell", bundle: nil)
+		tableView.register(nib, forCellReuseIdentifier: "eventCell")
+		let cell = tableView.dequeueReusableCell(withIdentifier: "eventCell", for: indexPath) as! EventTableViewCell
+		cell.cellTitle.text = "Cell " + String(indexPath.item)
+		return cell
+	}
+	
+	
 }
 
