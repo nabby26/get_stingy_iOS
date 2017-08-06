39df7962cb352aea91b0bc1da5b63d9a2f9fd9bf
blacksfk <s3333113@student.rmit.edu.au>
2017-08-06 10:17:06 +1000

    finished model

--- get_stingy/MockData.swift
+++ get_stingy/MockData.swift
@@ -9,5 +9,20 @@
 import UIKit
 
 class MockData: NSObject {
-
+    public var name:String
+    public var desc:String
+    public var lat:Double
+    public var long:Double
+    public var time:String
+    public var date:String
+    
+    // constructor
+    init(name:String, desc:String, lat:Double, long:Double, time:String, date:String) {
+        self.name = name
+        self.desc = desc
+        self.lat = lat
+        self.long = long
+        self.time = time
+        self.date = date
+    }
 }
